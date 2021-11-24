6dec789c84068f353b4fa202afc0c089947c66c7
kirdiyk <kirka.aka112@ya.ru>
2021-11-24 22:52:14 +0300

    правки базовые критерии

---
+++ project/src/components/sign-in/sign-in.tsx
@@ -0,0 +1,77 @@
+import {FormEvent, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {loginAction} from '../../store/actions-api';
+import {Authentication} from '../../types/authentication';
+import Footer from '../footer/footer';
+import Logo from '../logo/logo';
+import { EMAIL_PATTERN, PASSWORD_PATTERN } from '../../const';
+
+function SignIn() : JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(true);
+  const [isValidPassword, setIsValidPassword] = useState<boolean>(true);
+  const dispatch = useDispatch();
+
+  const onSubmit = (authData: Authentication) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (email !== null && password !== null) {
+      onSubmit({
+        login: email,
+        password: password,
+      });
+    }
+  };
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <Logo/>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">Sign in</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <form className="sign-in__form" onSubmit={handleSubmit}>
+          {!isValidEmail &&
+          <div className="sign-in__message">
+            <p>Please enter a valid email address</p>
+          </div>}
+
+          {!isValidPassword &&
+          <div className="sign-in__message">
+            <p>
+              Please enter a valid password
+            </p>
+          </div>}
+          <div className="sign-in__fields">
+            <div className={`sign-in__field ${(!isValidEmail || !email) && 'sign-in__field--error'}`}>
+              <input id="user-email" className="sign-in__input" type="email" placeholder="Email address" name="user-email" required pattern={EMAIL_PATTERN.source} value={email} onChange={(evt) => setEmail(evt.currentTarget.value)} onBlur={(evt) => setIsValidEmail(!evt.currentTarget.validity.patternMismatch)}/>
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+            </div>
+            <div className="sign-in__field">
+              <input id="user-password" className="sign-in__input" type="password" placeholder="Password" name="user-password" required pattern={PASSWORD_PATTERN.source} value={password} onChange={(evt) => setPassword(evt.currentTarget.value)} onBlur={(evt) => setIsValidPassword(!evt.currentTarget.validity.patternMismatch)}/>
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+            </div>
+          </div>
+          <div className="sign-in__submit">
+            <button className="sign-in__btn" type="submit">
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ project/src/const.tsx
@@ -0,0 +1,118 @@
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  OwnList = '/mylist',
+  AddReview = '/review',
+  Film = '/films/',
+  Player = '/player/',
+  Page404 = '/page-not-found',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export enum TabType {
+  Overview = 'Overview',
+  Details = 'Details',
+  Reviews = 'Reviews',
+}
+
+export enum Rating {
+  Bad = 'Bad',
+  Normal = 'Normal',
+  Good = 'Good',
+  VeryGood = 'Very good',
+  Awesome = 'Awesome',
+}
+
+export enum APIRoute {
+  Promo = '/promo',
+  Films = '/films',
+  Login = '/login',
+  Logout = '/logout',
+  Similar = '/similar',
+  Comments = '/comments',
+  Favorite = '/favorite',
+}
+
+export const initialFilm = {
+  id: 0,
+  name:	'',
+  posterImage:	'',
+  previewImage:	'',
+  backgroundImage:	'',
+  backgroundColor:	'',
+  description: '',
+  rating:	0,
+  scoresCount:	0,
+  director:	'',
+  starring: [],
+  runTime:	0,
+  genre:	'',
+  released:	0,
+  isFavorite:	false,
+  videoLink:	'',
+  previewVideoLink:	'',
+};
+
+export const initialUser = {
+  id: 0,
+  email: '',
+  name: '',
+  avatarUrl: '',
+  token: '',
+};
+
+export enum RatingGrades {
+  Bad = 0,
+  Normal = 3,
+  Good = 5,
+  VeryGood = 8,
+  Awesome = 10,
+}
+
+export const BACKGROUND_REVIEW_FORM = 'rgba(255, 255, 255, 0.4)';
+
+export const LIMIT_COUNTER_START = 1;
+
+export const CLASS_TAB_ACTIVE = 'film-nav__item--active';
+
+export const ALL_GENRES = 'All genres';
+
+export const CLASS_GENRE_ACTIVE = 'catalog__genres-item--active';
+
+export const STEP_FILMS = 8;
+
+export const SIMILAR_FILM_NUMBER = 4;
+
+export const GRADES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const USER_GRADES_NULL = 0;
+
+export const COMMENT_LENGHT_MIN = 50;
+
+export const COMMENT_LENGHT_MAX = 400;
+
+export const EMAIL_PATTERN = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+export const PASSWORD_PATTERN = /(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9]+/;
+
+
